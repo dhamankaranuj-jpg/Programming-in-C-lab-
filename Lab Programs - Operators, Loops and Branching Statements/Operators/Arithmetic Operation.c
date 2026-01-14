7447a3a7cadd929d0af11d11db147991abc00e84
dhamankaranuj-jpg <dhamankaranuj@gmail.com>
2026-01-14 08:50:06 +0530

    Create Arithmetic Operation.c

---
+++ Lab Programs - Operators, Loops and Branching Statements/Operators/Arithmetic Operation.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){int num1,num2;int sum,diffrence,product,remainder;int quotient;
+		  
+		  
+		  scanf("%d%d",&num1,&num2);
+		  sum= num1+num2;
+		   diffrence= num1-num2;
+		   product= num1*num2;
+		   quotient= num1/num2;
+		   remainder= num1%num2;
+
+
+		  
+		    printf("%d\n",sum);
+				 printf("%d\n",diffrence);
+				 printf("%d\n",product);
+				 printf("%d\n",quotient);
+				 printf("%d\n",remainder);
+
+				return 0;
+		  }
+		  
